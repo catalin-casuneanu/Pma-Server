e27b2ba050142b4199e41f7aafa4c17c68a86a02
Catalin Casuneanu <catalin.casuneanu@jay-software.com>
2022-04-06 09:52:54 +0300

    feat: seeder routes

--- src/index.js
+++ src/index.js
@@ -22,13 +22,31 @@ app.use(cors());
 app.use(morgan("combined"));
 
 app.get("/location", async (req, res) => {
-  locations = await seedFranchiseLocations();
+  locations = await GetFranchiseLocations();
   res.status(200).json(locations);
 });
 
 app.get("/owner", async (req, res) => {
-  owners = await SeedFranchiseOwners();
+  owners = await GetFranchiseOwners();
   res.status(200).json(owners);
+});
+
+app.post("/seed", async (res, req) => {
+  const type = res.body.type;
+
+  switch (type) {
+    case "location":
+      const locations = await seedFranchiseLocations();
+      res.status(20).json({ message: "ok", data: locations });
+      break;
+    case "owner":
+      const owners = await SeedFranchiseOwners();
+      res.status(20).json({ message: "ok", data: owners });
+      break;
+    default:
+      res.status(400).json({ message: "Unknown type." });
+      break;
+  }
 });
 
 app.listen(3000, () => {
